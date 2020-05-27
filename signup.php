19ae9d119d4e4d9896f80af2108dafbbfee7976b
rupali-talele <rupali.talele1995@gmail.com>
2020-05-26 23:18:00 -0700

    Dynamic sign up page
    Created dynamic sign up page to sign up as student or teacher using javascript

--- signup.php
+++ signup.php
@@ -15,72 +15,19 @@
 <!-- content -->
     <div class="content">
     <main class="main container">
-      <div class="signupdiv">
-      <form class="signup_form" action="signup_database.php" method="post">
-          
-        <!-- Name -->
-          <div class="signup_form-item">
-            <label class="signup_label" for="fname">First Name</label>
-          <!-- </div>
-          
-          <div class="form-item"> -->
-             <input class="signup_inputbox" type="text" id="fname" name="fname" placeholder=" First Name" />
-          </div>
+        <div class="signupdiv">
+            <form action="" class="signupas_form">
+                <label for="signup_as" class="signup_label">Sign up as-</label>
+                <input type="button" class="signup_button" id ="student-btn" value="STUDENT">
+                <input type="button" class="signup_button" id = "teacher-btn" value="TEACHER">
 
-          <div class="signup_form-item">
-            <label class="signup_label" for="lname">Last Name</label>
-          <!-- </div>
-          
-          <div class="form-item"> -->
-             <input class="signup_inputbox" type="text" id="lname" name="lname" placeholder=" Last Name" />
-          </div>     
-          
-        <!-- username -->
-          <div class="signup_form-item">
-            <label class="signup_label" for="userid">Username</label>
-          <!-- </div>
-          
-          <div class="form-item"> -->
-             <input class="signup_inputbox" type="text" id="userid" name="userid" placeholder=" Username" />
-          </div>          
-        <!-- password -->
-
-        <div class="signup_form-item">
-            <label class="signup_label" for="pwd">Password</label>
-        <!-- </div>
-          
-        <div class="form-item"> -->
-          <input class="signup_inputbox" type="password" name="pwd" id="pwd" placeholder=" ***********" />
+            </form>
         </div>
-
-        <div class="signup_form-item">
-            <label class="signup_label" for="pwd">Retype Password</label>
-        <!-- </div>
-          
-        <div class="form-item"> -->
-          <input class="signup_inputbox" type="password" name="pwd" id="pwd" placeholder=" ***********" />
-        </div>        
- 
-        <!-- signup as  -->
-
-        <!-- submit button  -->
-
-        <div class="signup_button_div">
-          <input class="signup_button" type="submit" value="SIGN UP" />
-        </div>
-
-        
-
-
-      </form>
-      </div>
-      
-     </main>
-     <!-- <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br> -->
-    </div>
+    </main>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
     <div>
       <?php include 'footer.php' ?>
     </div>  
-
+    <script src="signup.js"></script>
   </body>
 </html>
